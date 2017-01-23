f9541f5d3c6bf0cf376c761fe7da7f33a17280f4
Umair Ejaz <umairejaz.ch@gmail.com>
2017-01-24 02:16:16 +0400

    test cases added for dock and ship

--- dock/tests.py
+++ dock/tests.py
@@ -1,3 +1,79 @@
+import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from dock.models import Dock, Job
+from ship.models import Ship, Container
+
+
+class ShipTestCase(TestCase):
+
+    def setUp(self):
+        Ship.objects.create(
+            name="ship 1",
+            arrival_time=datetime.datetime.now(),
+            description="Historic overview"
+        )
+
+        Ship.objects.create(
+            name="ship 2",
+            arrival_time=datetime.datetime.now(),
+            description="Historic overview"
+        )
+
+        Container.objects.create(
+            name="Container 1",
+            hazard='1',
+            ship=Ship.objects.get(id=1)
+        )
+
+        Container.objects.create(
+            name="Container 2",
+            hazard='2',
+            ship=Ship.objects.get(id=2)
+        )
+
+        Dock.objects.create(
+            name="Dock 1"
+        )
+
+        Dock.objects.create(
+            name="Dock 2"
+        )
+
+        Job.objects.create(
+            dock=Dock.objects.get(id=1),
+            ship=Ship.objects.get(id=1),
+            job_status='2',
+            start_time=datetime.datetime.now()
+        )
+
+        Job.objects.create(
+            dock=Dock.objects.get(id=2),
+            ship=Ship.objects.get(id=2),
+            job_status='2',
+            start_time=datetime.datetime.now()
+        )
+
+    def test_two_ships_in_one_dock(self):
+        """
+        check can status of two ships be active on the same dock
+        """
+        count = Job.objects.filter(
+            dock=Dock.objects.get(id=2),
+            job_status='2'
+        ).count()
+        self.assertEqual(count, 1)
+
+        Job.objects.create(
+            dock=Dock.objects.get(id=2),
+            ship=Ship.objects.get(id=1),
+            job_status='2',
+            start_time=datetime.datetime.now()
+        )
+
+        count = Job.objects.filter(
+            dock=Dock.objects.get(id=2),
+            job_status='2'
+        ).count()
+        self.assertEqual(count, 1)
--- ship/tests.py
+++ ship/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from dock.models import Dock, Job
+from ship.models import Ship, Container
+from employee.models import Employee
+
+
+class ShipTestCase(TestCase):
+
+    def setUp(self):
+        # Test definitions as before.
+        Ship.objects.create(
+            name="ship 1",
+            arrival_time=datetime.datetime.now(),
+            description="Historic overview"
+        )
+
+        Ship.objects.create(
+            name="ship 2",
+            arrival_time=datetime.datetime.now(),
+            description="Historic overview"
+        )
+
+        Container.objects.create(
+            name="Container 1",
+            hazard='1',
+            ship=Ship.objects.get(id=1)
+        )
+
+        Container.objects.create(
+            name="Container 2",
+            hazard='2',
+            ship=Ship.objects.get(id=2)
+        )
+
+        Dock.objects.create(
+            name="Dock 1"
+        )
+
+        Dock.objects.create(
+            name="Dock 2"
+        )
+
+        Job.objects.create(
+            dock=Dock.objects.get(id=1),
+            ship=Ship.objects.get(id=1),
+            job_status='2',
+            start_time=datetime.datetime.now()
+        )
+
+        Job.objects.create(
+            dock=Dock.objects.get(id=2),
+            ship=Ship.objects.get(id=2),
+            job_status='2',
+            start_time=datetime.datetime.now()
+        )
+
+    def test_home_page_status(self):
+        """
+        call home page and check its status
+        """
+        client = Client()
+        response = client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_dock_detail_page_status(self):
+        """
+        call dock detail page and check its status
+        """
+        client = Client()
+        response = client.get(reverse('dock_detail', kwargs={'id': 1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_dock_detail_page_response(self):
+        """
+        call dock detail page, check its status
+        and check the returned dock object
+        """
+        client = Client()
+        response = client.get(reverse('dock_detail', kwargs={'id': 1}))
+        dock = Dock.objects.get(id=1)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['dock'].id, dock.id)
